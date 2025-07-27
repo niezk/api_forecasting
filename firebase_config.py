dbd86d2f7631d57b7838f390d0ad8c89dafc9b42
niezk <119299732+niezk@users.noreply.github.com>
2025-07-27 21:01:09 +0700

    env fixed

--- firebase_config.py
+++ firebase_config.py
@@ -1,12 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+import json
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-cred_path = os.path.join(BASE_DIR, "ServiceCredentials.json")
-# Only initialize if not already initialized
+# Load the service account from environment variable
+service_account_info = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+
+# Initialize Firebase
 if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_path)
+    cred = credentials.Certificate(service_account_info)
     firebase_admin.initialize_app(cred)
 
 # Export Firestore client
